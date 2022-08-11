9de2412e1668cd1054925308406153d000a7de68
onedayherenow <onedayherenow@gmail.com>
2022-08-11 16:59:24 -0400

    Full test suite hardhat tutorial

--- test/ExtendedToken.js
+++ test/ExtendedToken.js
@@ -10,7 +10,7 @@ const { expect } = require("chai");
 // Using this simplified your tests and makes them run faster, by taking
 // advantage of Hardhat's Network's snapshot functionality
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
-const { ethers } = require("ethers");
+const { ethers } = require("hardhat");
 
 // 'describe' is a Mocha function that allows you to organize your tests.
 // Having your tests organized makes debugging them easier.  All Mocha
@@ -62,9 +62,10 @@ describe("Token contract", function () {
             const ownerBalance = await hardhatToken.balanceOf(owner.address);
             expect(await hardhatToken.totalSupply()).to.equal(ownerBalance);
         });
-        });
+    });
 
-        describe("Transactions". function () 
+
+        describe("Transactions", function () {
         it("Should transfer tokens between accounts", async function () {
             const { hardhatToken, owner, addr1, addr2 } = await loadFixture(
                 deployTokenFixture
@@ -93,16 +94,28 @@ describe("Token contract", function () {
 
             // Transfer 50 tokens from addr1 to addr2
             // We use .connect(signer) to send a transaction from another account
-            await expect(hardhatToken.connect(addr1.transfer(addr2.address, 50))
+            await expect(hardhatToken.connect(addr1).transfer(addr2.address, 50))
             .to.emit(hardhatToken, "Transfer")
             .withArgs(addr1.address, addr2.address, 50);
         });
 
-        
-        })
-            
-        }))
 
+        it("Should fail if sender doesn't have enough tokens", async function () {
+            const {hardhatToken, owner, addr1 } = await loadFixture(
+                deployTokenFixture
+            );
+            const initialOwnerBalance = await hardhatToken.balanceOf(owner.address);
 
-    })
-})
+            // Try to send 1 token from addr1 (0 tokens) to owner (1000 tokens)
+            // 'require' will evaluate false and revert the transaction
+            await expect(
+                hardhatToken.connect(addr1).transfer(owner.address, 1)
+            ).to.be.revertedWith("Not enough tokens");
+
+            // Owner balance shouldn't have changed
+            expect(await hardhatToken.balanceOf(owner.address)).to.equal(
+                initialOwnerBalance
+            ); 
+        });    
+    });
+});
